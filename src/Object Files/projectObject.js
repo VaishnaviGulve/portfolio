fa3fd3920fb3b7f8cf80a9b60846e61320d6462a
KaranGulve4342 <karangulve4342@gmail.com>
2025-09-18 12:35:31 +0530

    Refactor the Project content with renaming the images

--- src/Object Files/projectObject.js
+++ src/Object Files/projectObject.js
@@ -3,45 +3,59 @@ import { v4 as uuid } from "uuid";
 export const projectData = [
   {
     id: uuid(),
-    title: "Agency",
-    image: "./asstes/agency1.png",
-    description: "Create A Amazing Router Agency website",
-    stacks: ["React", "CSS", "API", "Framer Motion"],
-    liveSite: "https://agency-website72.netlify.app/",
-   design: "Ilma",
-    
+    title: "Travel App & Web Design",
+    image: "./asstes/travel-app-image.jpg",
+    description:
+      "A seamless travel platform design offering personalized trip planning, destination insights, and booking features.",
+    stacks: [
+      "Figma",
+      "UI/UX Design Principles",
+      "Prototyping",
+      "Icon8",
+    ],
+    liveSite:
+      "https://www.figma.com/design/QIfPCbGjAZzn1P3iULXqqh/PROJECTS?node-id=145-2099&t=CmkrS4VUzTQQivUV-1",
+    github:
+      "https://www.figma.com/design/QIfPCbGjAZzn1P3iULXqqh/PROJECTS?node-id=145-2099&t=CmkrS4VUzTQQivUV-1",
+    design: "Vaishnavi",
   },
   {
     id: uuid(),
-    title: "Event.io ",
-    image: "./asstes/event.png",
-    description: "Interactive Eventio",
-    stacks: ["React","Framer Motion","Router", "Styled Component"],
-    liveSite: "https://wonderful-epic-34d8da.netlify.app/",
-    github: "https://github.com/Ilmakhan7/Event.io",
-    design: "Ilma",
-    
+    title: "Tech Startup Web Design",
+    image: "./asstes/tech-startup-image.jpg",
+    description:
+      "A professional website concept for a modern tech startup, showcasing innovation, expertise, and solutions with a sleek UI.",
+    stacks: ["Figma", "UI/UX Design Principles", "Google Fonts", "Icon8"],
+    liveSite:
+      "https://www.figma.com/proto/QIfPCbGjAZzn1P3iULXqqh/PROJECTS?page-id=1%3A565&node-id=137-6846&p=f&viewport=132%2C116%2C0.09&t=nH7TdGmAFybF5Rkc-1&scaling=scale-down-width&content-scaling=fixed",
+    github:
+      "https://www.figma.com/proto/QIfPCbGjAZzn1P3iULXqqh/PROJECTS?page-id=1%3A565&node-id=137-6846&p=f&viewport=132%2C116%2C0.09&t=nH7TdGmAFybF5Rkc-1&scaling=min-zoom&content-scaling=fixed",
+    design: "Vaishnavi",
   },
   {
     id: uuid(),
-    title: "Apple E-Commerce ",
-    image: "./asstes/apple.png",
-    description: "Discover the Apple of Online Shopping.",
-    stacks: ["Framer Motion", "Javascript"],
-    liveSite: "https://apple-website9168d2.netlify.app/",
-    github: "https://github.com/Ilmakhan7/Apple-website",
-    design: "Ilma",
+    title: "FogOFF™ Web Design",
+    image: "./asstes/flogoff-image.jpg",
+    description:
+      "A modern ski goggle brand website highlighting products, vision, and customer trust with a clean, user-friendly interface.",
+    stacks: ["Webflow", "UI/UX Design Principles", "Google Fonts"],
+    liveSite:
+      "https://preview.webflow.com/preview/vaishnavis-stupendous-site-4f1720?utm_medium=preview_link&utm_source=designer&utm_content=vaishnavis-stupendous-site-4f1720&preview=d155227bb60f44d6a9ad9d4d53bb285b&workflow=preview",
+    github:
+      "https://preview.webflow.com/preview/vaishnavis-stupendous-site-4f1720?utm_medium=preview_link&utm_source=designer&utm_content=vaishnavis-stupendous-site-4f1720&preview=d155227bb60f44d6a9ad9d4d53bb285b&workflow=preview",
+    design: "Vaishnavi",
   },
- 
   {
     id: uuid(),
-    title: "Zeee Studio Fashion",
-    image: "./asstes/zeestudio.png",
-    description: "Create A Amazing Fashion Sudio",
-    stacks: ["React", "CSS", "gsap", "Styled Component"],
-    liveSite: "https://zee-studio-fashion.netlify.app/",
-    github: "https://github.com/Ilmakhan7/fashion-studio",
-    design: "Ilma",
+    title: "Team App Web Design",
+    image: "./asstes/team-app-image.jpg",
+    description:
+      "A modern web design concept for a team collaboration app with clean UI and intuitive layout.",
+    stacks: ["Figma", "UI/UX Design Principles", "Google Fonts"],
+    liveSite:
+      "https://www.figma.com/design/QIfPCbGjAZzn1P3iULXqqh/PROJECTS?node-id=1-1736&t=CmkrS4VUzTQQivUV-1",
+    github:
+      "https://www.figma.com/proto/QIfPCbGjAZzn1P3iULXqqh/PROJECTS?page-id=1%3A1736&node-id=1-2950&p=f&viewport=-358%2C494%2C0.06&t=nsDh1z1zaxmltWlL-1&scaling=min-zoom&content-scaling=fixed",
+    design: "Vaishnavi",
   },
-
 ];
